4e609c8bd89fb1f4cfe4b70dc4e708d430d43634
richard-iths <richard.forsberg@iths.se>
2021-02-09 10:48:38 +0100

    Added getter to vuex for colors

--- src/components/SuggestionList.vue
+++ src/components/SuggestionList.vue
@@ -1,24 +1,27 @@
 <template>
   <ul>
-    <li 
-      v-for="color in colors" 
-      :key="color"
-      @click="$emit('click', color)"
-    >{{color}}</li>
+    <li v-for="color in getMatch" :key="color" @click="$emit('click', color)">
+      {{ color }}
+    </li>
   </ul>
 </template>
 
 <script>
 export default {
-  props: ['match'],
-  data(){ return {
-    colors: ['red','green','blue'] // Change this to a vuex getter that filters based on this.match
-  }},
-}
+  props: ["match"],
+  computed: {
+    getMatch() {
+      return this.$store.getters.matchColor(this.match);
+    },
+  },
+  // data(){ return {
+  //   colors: ['red','green','blue'] // Change this to a vuex getter that filters based on this.match
+  // }},
+};
 </script>
 
 <style scoped lang="scss">
-ul{
+ul {
   position: absolute;
 }
 </style>
